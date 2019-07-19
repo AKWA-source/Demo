6267cb65f40a2980adf13da6320b5a066fefd1a5
Akram <shaik-waseem.akram@atos.net>
2019-07-19 15:10:54 +0530

    First commit

---
+++ Automation/src/functionalLibraries/Screenshot.java
@@ -0,0 +1,33 @@
+package functionalLibraries;
+
+import java.io.File;
+import java.io.IOException;
+
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+
+public class Screenshot {
+
+	
+		 
+		public static void captureScreenShot(WebDriver ldriver){
+		// Take screenshot and store as a file format             
+		 File src=((TakesScreenshot)ldriver).getScreenshotAs(OutputType.FILE);           
+		try {
+		// now copy the  screenshot to desired location using copyFile method
+		 
+		FileUtils.copyFile(src, new File("./src/resources/Screenshots"+System.currentTimeMillis()+".png"));   } 
+		catch (IOException e)
+		 {
+		  System.out.println(e.getMessage());
+		 
+		 }
+		
+		  
+		 
+		}
+}
